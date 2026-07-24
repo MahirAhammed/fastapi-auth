05b13b0eea2c33b2d566bbef2396f70de8834da8
Mahir <itsmahir24@gmail.com>
2026-07-24 22:03:35 +0200

    refactor: move config.py to core dir

--- supabase_client.py
+++ supabase_client.py
@@ -1,6 +1,6 @@
 from supabase import create_client, Client
 from supabase.client import ClientOptions
-from config import SUPABASE_URL, SUPABASE_KEY
+from core.config import SUPABASE_URL, SUPABASE_KEY
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY,
     options=ClientOptions(
